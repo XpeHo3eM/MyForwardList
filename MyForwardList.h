0919111262a8cb6e4555b46bbab531a156486513
Никулин Павел <XpeHo3eM@gmail.com>
2024-04-20 22:18:33 +0400

    feat: Realized forward_list

--- MyForwardList.h
+++ MyForwardList.h
@@ -1,48 +1,62 @@
 ﻿#pragma once
-#include <iostream>
+
 
 
 template <class T>
 class MyForwardList {
+private:
+    struct Node {
+        T data;
+        Node* next;
+
+        Node(const T& data)
+            : data(data), next(nullptr)
+        {}
+    };
+
+    Node* head_ = nullptr;
+    Node* tail_ = nullptr;
+    size_t size_ = 0;
+
+
+    void clearDatas() {
+        while (head_ != nullptr) {
+            auto next = head_->next;
+
+            delete head_;
+            head_ = next;
+        }
+
+        size_ = 0;
+    };
+
+
 public:
     MyForwardList() = default;
-
 
     ~MyForwardList() {
         clearDatas();
     }
 
-
     MyForwardList(const MyForwardList& other) {
-        m_head = nullptr;
-        m_size = 0;
-        Node* oCurrent = other.m_head;
-
+        Node* oCurrent = other.head_;
+        
         while (oCurrent != nullptr) {
-            Node* newNode = new Node(oCurrent->value);
-
-            if (m_head == nullptr) {
-                m_head = newNode;
-                m_tail = m_head;
-            } else {
-                m_tail->next = newNode;
-                m_tail = newNode;
-            }
+            push_back(oCurrent->data);
 
             oCurrent = oCurrent->next;
-            ++m_size;
         }
     }
 
-
-    MyForwardList(const MyForwardList&& other) 
-        : m_head(other.m_head), m_tail(other.m_tail), m_size(other.m_size)
+    MyForwardList(MyForwardList&& other) noexcept
+        : head_(other.head_), 
+        tail_(other.tail_), 
+        size_(other.size_)
     {
-        other.m_head = nullptr;
-        other.m_tail = nullptr;
-        other.m_size = 0;
+        other.head_ = nullptr;
+        other.tail_ = nullptr;
+        other.size_ = 0;
     }
-
 
     MyForwardList& operator=(const MyForwardList& other) {
         if (this == &other) {
@@ -51,143 +65,201 @@ public:
 
         clearDatas();
 
-        Node* oCurrent = other.m_head;
+        Node* oCurrent = other.head_;
 
         while (oCurrent != nullptr) {
-            Node* newNode = new Node(oCurrent->value);
+            Node* newNode = new Node(oCurrent->data);
 
-            if (m_head == nullptr) {
-                m_head = newNode;
-                m_tail = m_head;
+            if (head_ == nullptr) {
+                head_ = newNode;
+                tail_ = head_;
             }
             else {
-                m_tail->next = newNode;
-                m_tail = newNode;
+                tail_->next = newNode;
+                tail_ = tail_->next;
             }
 
             oCurrent = oCurrent->next;
         }
-        m_size = other.m_size;
+        size_ = other.size_;
 
         return *this;
     }
 
-
-    MyForwardList& operator=(const MyForwardList&& other) {
+    MyForwardList& operator=(MyForwardList&& other) noexcept {
         if (this == &other) {
             return *this;
         }
 
-        clearDatas();
+        clearDatas(); 
 
-        m_head = std::move(other.m_head);
-        m_tail = std::move(other.m_tail);
-        m_size = other.m_size;
-        other.m_size = 0;
+        head_ = other.head_;
+        tail_ = other.tail_;
+        size_ = other.size_;
+
+        other.head_ = nullptr;
+        other.tail_ = nullptr;
+        other.size_ = 0;
 
         return *this;
     }
 
-
     MyForwardList(const std::initializer_list<T> list) {
         for (const auto& el : list) {
-            Node* newNode = new Node(el);
-
-            if (m_head == nullptr) {
-                m_head = newNode;
-                m_tail = m_head;
-            } else {
-                m_tail->next = newNode;
-                m_tail = newNode;
-            }
-
-            ++m_size;
+            push_back(el);
         }
     }
-
 
     size_t size() {
-        return m_size;
+        return size_;
     }
 
+    bool is_empty() const {
+        return size_ == 0;
+    }
 
-    void insert(const T& value) {
-        Node* newNode = new Node(value);
+    void push_front(const T& data) {
+        Node* newNode = new Node(data);
 
-        if (m_head == nullptr) {
-            m_head = newNode;
-            m_tail = m_head;
+        if (head_ == nullptr) {
+            head_ = newNode;
         } else {
-            m_tail->next = newNode;
-            m_tail = newNode;
+            newNode->next = head_;
+            head_ = newNode;
         }
 
-        ++m_size;
+        ++size_;
     }
 
+    void push_back(const T& data) {
+        Node* newNode = new Node(data);
 
-    void remove(const T& value) {
-        while (m_head != nullptr && m_head->value == value) {
-            Node* next = m_head->next;
-
-            delete m_head;
-            m_head = next;
-
-            --m_size;
+        if (head_ == nullptr) {
+            head_ = newNode;
+            tail_ = head_;
+        } else {
+            tail_->next = newNode;
+            tail_ = tail_->next;
         }
 
-        if (m_head == nullptr) {
-            m_tail = nullptr;
+        ++size_;
+    }
+
+    void remove(const T& data) {
+        while (head_ != nullptr && head_->data == data) {
+            Node* next = head_->next;
+
+            delete head_;
+            head_ = next;
+
+            --size_;
+        }
+
+        if (head_ == nullptr) {
+            tail_ = nullptr;
 
             return;
         }
 
-        Node* prev = m_head;
-        Node* current = m_head->next;
+        Node* prev = head_;
+        Node* current = head_->next;
 
         while (current != nullptr) {
-            if (current->value == value) {
+            if (current->data == data) {
+                if (current == tail_) {
+                    tail_ = prev;
+                }
+
                 prev->next = current->next;
 
                 delete current;
                 current = prev->next;
 
-                --m_size;
-
-                if (current == m_tail) {
-                    m_tail = prev;
-                }
+                --size_;
             } else {
                 current = current->next;
             }
         }
     }
 
+    class Iterator {
+    public:
+        Iterator() : node(nullptr) {}
+        Iterator(Node* node) : node(node) {}
 
-    T* find(const T& value) const {
-        Node* current = m_head;
 
-        while (current != nullptr) {
-            if (current->value == value) {
-                return &current->value;
-            }
+        T& operator*() {
+            return node->data;
         }
 
-        return nullptr;
+
+        Iterator& operator++() {
+            node = node->next;
+
+            return *this;
+        }
+
+
+        bool operator!=(const Iterator& other) const {
+            if (node == nullptr && other.node == nullptr) {
+                return false;
+            }
+            if (node == nullptr || other.node == nullptr) {
+                return true;
+            }
+
+            return node->data != other.node->data;
+        }
+
+
+        bool is_null() const {
+            return node == nullptr;
+        }
+
+
+    private:
+        Node* node;
+
+        void assertNode() const {
+            if (node == nullptr) {
+                throw std::logic_error("Node is nullptr");
+            }
+        }
+    };
+
+    Iterator begin() const {
+        return Iterator(head_);
     }
 
+    Iterator end() const {
+        return Iterator(nullptr);
+    }
+
+    Iterator find(const T& data) const {
+        Node* current = head_;
+
+        while (current != nullptr) {
+            if (current->data == data) {
+                return Iterator(current);
+            }
+
+            current = current->next;
+        }
+
+        return Iterator(nullptr);
+    }
 
     bool operator==(const MyForwardList& other) const
     {
-        if (m_size != other.m_size) {
+        if (size_ != other.size_) {
             return false;
         }
 
-        Node* current  = m_head;
-        Node* oCurrent = other.m_head;
+        Node* current  = head_;
+        Node* oCurrent = other.head_;
 
         while (current != nullptr) {
-            if (current->value != oCurrent->value) {
+            if (current->data != oCurrent->data) {
                 return false;
             }
 
@@ -197,32 +269,4 @@ public:
 
         return true;
     }
-
-
-
-private:
-    struct Node {
-        T value;
-        Node* next;
-
-        Node(const T& value) 
-            : value(value), next(nullptr) 
-        {}
-    };
-
-    Node* m_head = nullptr;
-    Node* m_tail = nullptr;
-    size_t m_size = 0;
-
-
-    void clearDatas() {
-        while (m_head != nullptr) {
-            Node* next = m_head->next;
-
-            delete m_head;
-            m_head = next;
-        }
-
-        m_size = 0;
-    };
 };
--- main.cpp
+++ main.cpp
@@ -1,18 +1,83 @@
 #include <iostream>
 #include "MyForwardList.h"
-#include <forward_list>
+
+
+
+template<class T>
+void print(const T& list) {
+	if (list.is_empty()) {
+		std::cout << "List is empty" << std::endl;
+	}
+
+	for (const auto& el : list) {
+		std::cout << el << " ";
+	}
+	std::cout << std::endl;
+}
+
 
 
 int main() {
+	std::cout << "Default constructor (list):\t";
 	MyForwardList<int> list;
-
 	for (int i = 0; i < 5; ++i) {
-		list.insert(i);
+		list.push_back(i);
 	}
+	print(list);
 
+
+	std::cout << "After copy constructor (list2):\t";
 	auto list2 = list;
-	list2.insert(42);
+	print(list2);
 
-	MyForwardList<int> list3(list);
-	list3 = list2;
+	std::cout << "After push_back (list2):\t";
+	list2.push_back(42);
+	print(list2);
+
+	std::cout << "list == list2:\t" << std::boolalpha << (list == list2) << std::endl;
+
+
+	std::cout << "Initializer list constructor (list3):\t";
+	MyForwardList<int> list3{ 11, 12, 13 };
+	print(list3);
+
+
+	std::cout << "Copy operator= (list2 = list3):\t";
+	list2 = list3;
+
+	print(list3);
+
+	std::cout << "List3 find:\t" << *list3.find(12) << std::endl;
+	std::cout << "List3 after remove:\t";
+	list3.remove(12);
+	print(list3);
+
+	std::cout << "List2 after removed from list3:\t";
+	print(list2);
+
+	std::cout << "List3 added elements:\t";
+	list3.push_back(11);
+	list3.push_front(11);
+	print(list3);
+
+	std::cout << "List3 after remove duplicated elements:\t";
+	list3.remove(11);
+	print(list3);
+
+	std::cout << "List4 move constructor from list:\t";
+	MyForwardList<int> list4 = std::move(list);
+	print(list4);
+	std::cout << "List after move constructor to list4:\t";
+	print(list);
+
+	std::cout << "List move operator= from list4:\t";
+	list = std::move(list4);
+	print(list);
+	std::cout << "List4 after move operator= to list:\t";
+	print(list4);
+
+
+	std::cout << "Initializer list constructor double (listD):\t";
+	MyForwardList<double> listD{ 11.2, 12.6, 13.11 };
+	print(listD);
 }
